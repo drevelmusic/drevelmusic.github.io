ef8d78241cd3c0b96e23862b62e4d3be5f69b33e
drevelmusic <alexdrevel@gmail.com>
2026-07-22 00:33:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,3 +80,13 @@
         } else {
             sessionStorage.setItem('logoAnimated', 'true');
         }
+
+    // Тень шапки при скролле
+    window.addEventListener('scroll', function() {
+        const header = document.querySelector('header');
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
